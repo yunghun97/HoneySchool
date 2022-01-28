29340ff8ca9eff7782cdd49ad5ab3309652bbcf5
recul12 <recul12@naver.com>
2022-01-28 13:07:21 +0900

    Edit : BoardController 수정

--- Backend/src/main/java/com/ssafy/honeySchool/api/controller/BoardController.java
+++ Backend/src/main/java/com/ssafy/honeySchool/api/controller/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
 import java.sql.SQLException;
 import java.util.List;
 
