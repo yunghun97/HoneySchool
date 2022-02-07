4622004bff9444aca85c6293bd8cf3614958ec16
recul12 <recul12@naver.com>
2022-02-07 20:28:52 +0900

    fix : Fix HoneySchoolApplication

--- Backend/src/main/java/com/ssafy/honeySchool/HoneySchoolApplication.java
+++ Backend/src/main/java/com/ssafy/honeySchool/HoneySchoolApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {"com.ssafy.honeySchool.db.repository"})
-@EntityScan(basePackages = {"com.ssafy.honeySchool.db.entity"})
 public class HoneySchoolApplication {
 
 	public static void main(String[] args) {
