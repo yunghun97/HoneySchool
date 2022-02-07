340b250c7a8424a68e4c74e4775d674bfaac9f05
DESKTOP-KVCQHCD\SSAFY <shaian2@naver.com>
2022-02-07 14:19:27 +0900

    Feat : 댓글 API, 반 게시판 파일 업로드 구현

--- Backend/src/main/java/com/ssafy/honeySchool/HoneySchoolApplication.java
+++ Backend/src/main/java/com/ssafy/honeySchool/HoneySchoolApplication.java
@@ -2,8 +2,12 @@ package com.ssafy.honeySchool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.ssafy.honeySchool.db.repository"})
+@EntityScan(basePackages = {"com.ssafy.honeySchool.db.entity"})
 public class HoneySchoolApplication {
 
 	public static void main(String[] args) {
---
+++ Backend/src/main/java/com/ssafy/honeySchool/api/controller/CommentController.java
@@ -0,0 +1,87 @@
+package com.ssafy.honeySchool.api.controller;
+
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.db.entity.ClassBoard;
+import com.ssafy.db.entity.ClassComment;
+import com.ssafy.db.repository.ClassBoardRepository;
+import com.ssafy.db.repository.ClassCommentRepository;
+
+@CrossOrigin(origins = "http://localhost:8080/")
+@RestController
+@RequestMapping("/api/v1/board")
+public class CommentController {
+	
+	@Autowired
+	ClassBoardRepository classBoardRepository;
+	
+	@Autowired
+	ClassCommentRepository classCommentRepository;
+	
+	// 반 게시판 게시물의 댓글 가져오기
+	@GetMapping("/class/comment")
+    public ResponseEntity<List<ClassComment>> selectClassBoardComments(HttpServletRequest req){
+		String school = req.getParameter("school");		
+		int grade = Integer.parseInt(req.getParameter("grade"));
+		int classes = Integer.parseInt(req.getParameter("classes"));
+		int boardId = Integer.parseInt(req.getParameter("board_id"));
+		ClassBoard classBoard = classBoardRepository.findBySchoolAndGradeAndClassesAndId(school, grade, classes, boardId);
+		List<ClassComment> classComments = classCommentRepository.findClassCommentByClassBoard(classBoard);
+		return new ResponseEntity<List<ClassComment>>(classComments, HttpStatus.OK);
+    }
+	// 반 게시판 게시물에 댓글 쓰기
+	@PostMapping("/class/{boardId}/comment/")
+    public HttpStatus createComment(@PathVariable int boardId, @RequestBody ClassComment body){
+        ClassBoard classBoard = classBoardRepository.findById(boardId);
+//        body.setClassBoard(classBoard);
+        classCommentRepository.save(ClassComment.builder()
+        		.content(body.getContent())
+				.writer(body.getWriter())
+				.file_link(body.getFile_link())
+				.classBoard(classBoard)
+				.build());
+        return HttpStatus.OK;
+    }
+	// 댓글 삭제
+	// boardId를 아무거나 적어도 comment 삭제가 됨.
+	@DeleteMapping("/class/{boardId}/comment/{commentId}")
+    public HttpStatus deleteComment(@PathVariable int boardId, @PathVariable int commentId){
+		classCommentRepository.deleteById(commentId);
+        return HttpStatus.OK;
+    }
+	// 댓글 수정
+	// 댓글이 써진 게시물을 바꾸면 안됨
+	@Transactional
+	@PatchMapping("/class/{boardId}/comment/{commentId}")
+    public ResponseEntity<ClassComment> updateComment(@PathVariable int boardId, @PathVariable int commentId, @RequestBody ClassComment body){
+//		ClassBoard classBoard = classBoardRepository.findById(boardId);
+//		body.setClassBoard(classBoard);
+		ClassComment newComment = classCommentRepository.findById(commentId).get();
+		System.out.println(newComment.getContent());		
+		System.out.println(newComment);		
+		String content = body.getContent();
+		String writer = body.getWriter();
+		String file_link = body.getFile_link();
+        newComment.update(content, writer, file_link);
+        System.out.println(newComment.getContent());
+        System.out.println(newComment);		
+        return new ResponseEntity<ClassComment>(newComment, HttpStatus.OK);
+    }
+}
