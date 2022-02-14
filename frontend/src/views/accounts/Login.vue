e51bb431a42e65880982aa44deb68f3d9da981a6
DESKTOP-KVCQHCD\SSAFY <shaian2@naver.com>
2022-02-14 23:05:32 +0900

    Design: 로그인 레이아웃 적용

--- frontend/src/views/accounts/Login.vue
+++ frontend/src/views/accounts/Login.vue
@@ -1,38 +1,52 @@
 <template>
-  <div class="d-flex justify-content-center">
-    <div id="login-form" class="card">
-      <h3 class="card-header">로그인</h3>
-      <div class="card-body">
-        <Form
-          @submit="onSubmit"
-          :validation-schema="schema"
-          v-slot="{ errors }"
-        >
-          <div class="form-group">
-            <label>아이디</label>
-            <Field
-              name="user_id"
-              type="text"
-              class="form-control"
-              :class="{ 'is-invalid': errors.user_id }"
-            />
-            <div class="invalid-feedback">{{ errors.user_id }}</div>
-          </div>
-          <div class="form-group">
-            <label>비밀번호</label>
-            <Field
-              name="password"
-              type="password"
-              class="form-control"
-              :class="{ 'is-invalid': errors.password }"
-            />
-            <div class="invalid-feedback">{{ errors.password }}</div>
-          </div>
-          <div class="form-group">
-            <button type="submit" class="btn btn-primary mr-1">로그인</button>
-          </div>
-        </Form>
-        <router-link to="/signup">아직 회원이 아니신가요?</router-link>
+  <div id="outer-div">
+    <img
+      class="profile mb-3"
+      src="@/assets/board/all.png"
+      alt="login image"
+    />
+    <h3 class="my-4">로그인</h3>
+    <div class="d-flex justify-content-center">
+      <div id="login-form" class="card m-2 p-4">
+        <div class="card-body">
+          <Form
+            @submit="onSubmit"
+            :validation-schema="schema"
+            v-slot="{ errors }"
+          >
+            <div class="form-group py-2 d-flex flex-column align-items-start">
+              <label class="py-2 fs-5">아이디</label>
+              <Field
+                name="user_id"
+                type="text"
+                class="form-control"
+                :class="{ 'is-invalid': errors.user_id }"
+              />
+              <div class="invalid-feedback">{{ errors.user_id }}</div>
+            </div>
+            <div class="form-group py-2 d-flex flex-column align-items-start">
+              <label class="py-2 fs-5">비밀번호</label>
+              <Field
+                name="password"
+                type="password"
+                class="form-control"
+                :class="{ 'is-invalid': errors.password }"
+              />
+              <div class="invalid-feedback">{{ errors.password }}</div>
+            </div>
+            <div class="form-group d-grid gap-2 pt-4">
+              <button type="submit" class="btn bg-primary d-block">로그인</button>
+            </div>
+          </Form>
+        </div>
+      </div>
+    </div>
+    <div class="d-flex justify-content-center">
+      <div id="signup-button" class="card m-2 p-4 d-flex flex-column justify-content-center">
+        <p class="m-0">
+          아직 회원이 아니신가요?
+          <router-link to="/signup" class="text-decoration-none">회원가입 하세요.</router-link>
+        </p>
       </div>
     </div>
   </div>
@@ -79,10 +93,6 @@ export default {
 </script>
 
 <style>
-#login-form {
-  width: 800px;
-}
-
 .profile {
   width: 150px;
   height: 150px;
@@ -111,4 +121,30 @@ input[type="radio"] + label > img {
 input[type="radio"]:checked + label > img {
   transform: rotateZ(-10deg) rotateX(10deg);
 }
+
+/* pyun */
+#login-form {
+  width: 600px;
+  height: 450px;
+  background-color: rgb(255, 237, 169);
+  border-radius: 15px;
+}
+#signup-button {
+  width: 600px;
+  height: 100px;
+  border-radius: 15px;
+}
+#outer-div {
+  margin : 50px;
+}
+input {
+    height: 50px;
+}
+.btn {
+  height: 50px;
+}
+
+/* .pinkColor {
+  color: rgb(254, 183, 211);
+} */
 </style>
