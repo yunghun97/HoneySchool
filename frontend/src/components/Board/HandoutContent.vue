85a4afcff5d183cb9db681896881522d160d3cef
psomi0114 <psomi0114@gmail.com>
2022-02-15 12:40:28 +0900

    Edit: 사진첩 Carousel 적용

--- frontend/src/components/Board/HandoutContent.vue
+++ frontend/src/components/Board/HandoutContent.vue
@@ -1,38 +1,45 @@
 <template>  
-    <div class="content">
-        <h1>{{ currentarticle.board.title }}</h1>
-        <p v-if="currentarticle.board.user!==null">작성자 : {{ currentarticle.board.user.name }}</p>
-        <p>작성날짜 : {{currentarticle.board.date.split(" ")[0] }}</p>
-        <div v-for="content in currentarticle.board.content.split('\r')" :key="content" class="content-files">
-            <h2>{{ content }}</h2>
-        </div>
-        <div v-if="currentarticle.files.length > 0" class="content-files">
-            <p>첨부파일</p>
-            <div v-for="idx in currentarticle.files.length" :key="idx">
-                <a :href="`http://localhost:9999/static/uploads/${currentarticle.files[idx-1].stored_file_path}`">첨부파일 {{idx}}</a>
-            </div>
-        </div>
+  <div class="content">
+    <h1>{{ currentarticle.board.title }}</h1>
+    <p v-if="currentarticle.board.user!==null">작성자 : {{ currentarticle.board.user.name }}</p>
+    <p>작성날짜 : {{currentarticle.board.date.split(" ")[0] }}</p>
+    <div v-for="content in currentarticle.board.content.split('\r')" :key="content" class="content-files">
+      <h2>{{ content }}</h2>
     </div>
+    <div v-if="currentarticle.files.length > 0" class="content-files">
+      <p class="filelist"><img src="@/assets/board/handout.png" alt="" class="fileimg"> 첨부 파일 목록</p>
+      <div v-for="idx in currentarticle.files.length" :key="idx">
+        <img src="@/assets/board/file.png" alt="" class="fileimg"><a :href="`http://localhost:9999/static/uploads/${currentarticle.files[idx-1].stored_file_path}`"> 첨부파일 {{idx}}</a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 
 export default {
-    name: "HandoutContent",
-    props: ['currentarticle'],
+  name: "HandoutContent",
+  props: ['currentarticle'],
 }
 </script>
 <style scoped>
 .content {
-    padding-top: 20vh;
-    margin-bottom: 50px;
+  padding-top: 20vh;
+  margin-bottom: 50px;
 }
 .content > p {
-    margin-right: 25vh;
-    text-align: right;
+  margin-right: 25vh;
+  text-align: right;
 }
 .content-files {
-    text-align: left;
-    margin-left: 50px;
+  text-align: left;
+  padding-left: 80px;
+}
+.fileimg {
+  width: 30px;
+  height: 30px;
+}
+.filelist {
+  margin-top: 20px;
 }
 </style>
