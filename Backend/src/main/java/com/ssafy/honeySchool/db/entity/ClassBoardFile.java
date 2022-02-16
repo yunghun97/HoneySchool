b3b57504b46ed9ff376c87e75d02ee901996b2e8
gwon <yunghun97@naver.com>
2022-02-16 10:17:45 +0900

    Fix : 220216 파일 저장경로 위치 Sysout

--- Backend/src/main/java/com/ssafy/honeySchool/db/entity/ClassBoardFile.java
+++ Backend/src/main/java/com/ssafy/honeySchool/db/entity/ClassBoardFile.java
@@ -16,10 +16,14 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Builder
 @Getter
+@Setter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class ClassBoardFile {
